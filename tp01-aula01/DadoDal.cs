ef4d7f4028d24da2b2467749a64f012d2b5748a9
rita <maria.rita27012@gmail.com>
2022-08-09 10:46:11 -0300

    Adicionar arquivos de projeto.

---
+++ tp01-aula01/DadoDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp01_aula01
+{
+    internal class DadoDal
+    {
+    }
+}
